015d1985649731a26ebdfaefc9eb4388bc8fde8f
Michael Neuhaus <michael.neuhaus@gmx.de>
2019-03-21 17:23:14 +0100

    added JS version

---
+++ charakters/charakters.js
@@ -0,0 +1,18 @@
+// Fake-Movie-Hacking
+function output() {
+    
+        var r = Math.floor((Math.random() * 122) + 1);
+    
+        var s = String.fromCharCode(r);
+        process.stdout.write(s);
+
+        wait(5);
+    
+}
+
+function wait(time) {
+    setTimeout(output, time);
+}
+
+wait(5);
+
