dd101cf475fab9610b33d0030d5ad76d29e020a8
Michael Neuhaus <XSilent@users.noreply.github.com>
2019-03-21 17:23:59 +0100

    removed empty lines

--- charakters/charakters.js
+++ charakters/charakters.js
@@ -1,13 +1,11 @@
 // Fake-Movie-Hacking
 function output() {
-    
         var r = Math.floor((Math.random() * 122) + 1);
     
         var s = String.fromCharCode(r);
         process.stdout.write(s);
 
         wait(5);
-    
 }
 
 function wait(time) {
